6c47fa9414692ff0a7b346e8cf9e45c597b09c21
Sarah Inwood <sninwood@gmail.com>
2018-09-26 13:23:04 +1200

    updates

--- src/filter_exposed_degs_no_annot.sh
+++ src/filter_exposed_degs_no_annot.sh
@@ -5,5 +5,5 @@ set -eu
 bin/bbmap/filterbyname.sh \
 in=data/sorted_fasta/asw_isoforms_by_length.fasta \
 include=t \
-names=output/exposed/dese2/degs_with_no_annot.txt \
+names=output/exposed/deseq2/degs_with_no_annot.txt \
 out=output/exposed/no_annot/degs_no_annot.fasta
