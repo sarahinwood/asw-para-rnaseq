8fdc7e9bb674de45059229e379ec623e1aec814c
Sarah <sninwood@gmail.com>
2018-09-26 12:38:26 +1200

    R scripts for DGE analysis

---
+++ src/stinging_asw_clustering_timecourse_genes.R
@@ -0,0 +1,39 @@
+library("Mfuzz")
+library("DESeq2")
+library("data.table")
+
+  ##function to calculate geometric mean
+gm_mean <- function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm = na.rm) / length(x))
+}
+
+  ##read in dds saved in previous script
+dds <- readRDS("output/deseq2/asw_timecourse/dds.rds")
+  ##filter for only abdo samples
+dds_abdo <- dds[,dds$Tissue == "Abdomen"]
+  ##read in list of sig gene names
+sig_gene_names <- fread("output/deseq2/asw_timecourse/timecourse_sig_gene_names.csv")
+
+  ##vst log transform data - absolute counts now changed so cannot compare between genes (only between samples for 1 gene)
+vst <- varianceStabilizingTransformation(dds_abdo, blind = FALSE)
+  ##make matrix of transformed data
+vst_matrix <- data.table(as.matrix(assay(vst)), keep.rownames = TRUE)
+  ##melt to make long table rather than wide
+long_vst_data <- melt(vst_matrix, id.vars = "rn", variable.name = "Sample_name", value.name = "vst")
+  ##make table of colData from dds object
+long_coldata <- data.table(as.data.frame(colData(dds_abdo)))
+
+  ##merge sample data with vst data
+merged_exp_coldata <- merge(long_vst_data, long_coldata, all.x = TRUE, all.y = FALSE)
+  ##generate table of mean vst values
+mean_vst <- merged_exp_coldata[,.(vst_mean = gm_mean(vst)),by=.(rn, Treatment)]
+  ##make long table a wide table instead
+mean_vst_wide <- dcast(mean_vst, rn~Treatment)
+  ##make matrix with gene names as row names
+expression_matrix <- as.matrix(data.frame(mean_vst_wide, row.names = "rn"))
+
+  ##linking treatment labels to treatments
+pheno_data <- data.frame(row.names = colnames(expression_matrix), treatment = colnames(expression_matrix))
+
+
+
---
+++ src/stinging_asw_deseq_analysis.R
@@ -0,0 +1,47 @@
+library("tximport")
+library("data.table")
+library("DESeq2")
+library("ggplot2")
+
+gene2tx <- fread("data/asw_Trinity.fasta.gene_trans_map", header = FALSE)
+tx2gene <- data.frame(gene2tx[, .(V2, V1)])
+
+  ##Find all salmon quant files
+quant_files <- list.files(path="output/asw", pattern = "quant.sf", full.names=TRUE, recursive = TRUE)
+  ##assign names to quant files from folder name
+names(quant_files) <- gsub(".*/(.+)_quant/.*", "\\1", quant_files)
+  ##import the salmon quant files (tx2gene links transcript ID to Gene ID - required for gene-level summarisation... 
+  ##for methods that only provide transcript level estimates e.g. salmon)
+txi <- tximport(quant_files, type = "salmon", tx2gene = tx2gene, dropInfReps=TRUE)
+  ##Import table describing samples
+sample_data <- fread("data/full_sample_key.csv")
+setkey(sample_data, Sample_name)
+
+  ##Create dds object and link to sample data
+dds <- DESeqDataSetFromTximport(txi, colData = sample_data[colnames(txi$counts)], design = ~1)
+  ##save dds as a file for import in clustering timecourse genes script
+saveRDS(dds, file = "output/deseq2/asw_timecourse/dds.rds")
+
+  ##Select only abdomen samples
+dds_abdo <- dds[,dds$Tissue == "Abdomen"]
+  ##convert to factors
+dds_abdo$Treatment <- factor(dds_abdo$Treatment)
+dds_abdo$Wasp_Location <- factor(dds_abdo$Wasp_Location)
+  ##add factors of ineterst to design
+design(dds_abdo) <- ~Wasp_Location+Treatment
+  ##run deseq, must specify reduced model for LRT test
+dds_abdo <- DESeq(dds_abdo, test = "LRT", reduced = ~Wasp_Location)
+  ##filter results on alpha
+dds_abdo_res <- results(dds_abdo, alpha = 0.05)
+ ##order based off padj
+ordered_dds_abdo_res <- dds_abdo_res[order(dds_abdo_res$padj),]
+
+  ##make list of sig genes
+sig_genes <- subset(dds_abdo_res, padj < 0.05)
+  ##make list of sig gene names
+sig_gene_names <- row.names(sig_genes)
+  ##save list of sig gene names
+fwrite(data.table(sig_gene_names), "output/deseq2/asw_timecourse/timecourse_sig_gene_names.csv")
+
+##plot counts for genes of interest, sub in name
+plotCounts(dds_abdo, "TRINITY_DN13642_c0_g1", intgroup = c("Treatment", "Wasp_Location"))
