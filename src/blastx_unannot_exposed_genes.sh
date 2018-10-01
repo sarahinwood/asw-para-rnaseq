0c20537c7855e5696cc6633b8f40a4e20dff7f43
Sarah Inwood <sninwood@gmail.com>
2018-10-01 14:58:15 +1300

    fixed blastx

--- src/blastx_unannot_exposed_genes.sh
+++ src/blastx_unannot_exposed_genes.sh
@@ -3,7 +3,7 @@
 set -eu
 
 blastx \
-	-query output/exposed/noannot/degs_no_annot.fasta \
+	-query output/exposed/no_annot/degs_no_annot.fasta \
 	-db bin/db/blastdb/refseq_protein/refseq_protein.pal \
 	-num_threads 50 \
-	-outfmt 6 > output/exposed/noannot/blastx.outfmt6
+	-outfmt 6 > output/exposed/no_annot/blastx.outfmt6
