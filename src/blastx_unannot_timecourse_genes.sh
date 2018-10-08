f4db56df1a8d2db8eefe94c7dad7ea1c452b18e6
Sarah Inwood <sninwood@gmail.com>
2018-10-08 22:45:54 +1300

    fixing blast timecourse script

--- src/blastx_unannot_timecourse_genes.sh
+++ src/blastx_unannot_timecourse_genes.sh
@@ -6,4 +6,4 @@ blastx \
 	-query output/asw_timecourse/no_annot/degs_no_annot.fasta \
 	-db bin/db/blastdb/nr/nr \
 	-num_threads 50 \
-	-outfmt 6 > output/exposed/no_annot/blastx.outfmt6
+	-outfmt 6 > output/asw_timecourse/no_annot/blastx.outfmt6
