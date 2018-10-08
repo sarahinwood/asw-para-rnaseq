0c0f1e8dbceee245352f852b954d5ce7046b1c58
Sarah Inwood <sninwood@gmail.com>
2018-10-08 14:24:51 +1300

    fixing blast script

--- src/blastx_unannot_exposed_genes.sh
+++ src/blastx_unannot_exposed_genes.sh
@@ -4,6 +4,6 @@ set -eu
 
 blastx \
 	-query output/exposed/no_annot/degs_no_annot.fasta \
-	-db bin/db/blastdb/refseq_protein/refseq_protein.pal \
+	-db bin/db/blastdb/nr/nr \
 	-num_threads 50 \
 	-outfmt 6 > output/exposed/no_annot/blastx.outfmt6
