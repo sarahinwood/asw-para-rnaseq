c1301f9a900cb461499c2b27e110d7b6aa3d2b66
Sarah Inwood <sninwood@gmail.com>
2018-10-18 13:57:04 +1300

    interproscan

--- src/interproscan_uncharannot_degs_timecourse.sh
+++ src/interproscan_uncharannot_degs_timecourse.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-bin/interproscan-5.24-63.0/interproscan.sh \
+bin/interproscan-5.31-70.0/interproscan.sh \
 --input output/asw_timecourse/interproscan/unchar_hypo_annot_degs.fasta \
 --seqtype n \
 --output-dir output/asw_timecourse/interproscan \
