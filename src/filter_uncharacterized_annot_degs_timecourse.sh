dfbedb57955881ca2cedd01b752fb5b048cfa907
Sarah Inwood <sninwood@gmail.com>
2018-10-24 12:55:40 +1300

    adding genes with no annot to interproscan

--- src/filter_uncharacterized_annot_degs_timecourse.sh
+++ src/filter_uncharacterized_annot_degs_timecourse.sh
@@ -5,5 +5,5 @@ set -eu
 bin/bbmap/filterbyname.sh \
 in=data/asw_Trinity.fasta \
 include=t \
-names=output/asw_timecourse/interproscan/unchar_hypo_annot_ids.txt \
-out=output/asw_timecourse/interproscan/unchar_hypo_annot_degs.fasta
+names=output/asw_timecourse/interproscan/interproscan_ids.txt \
+out=output/asw_timecourse/interproscan/interproscan_degs.fasta
--- src/interproscan_uncharannot_degs_timecourse.sh
+++ src/interproscan_uncharannot_degs_timecourse.sh
@@ -3,7 +3,7 @@
 set -eu
 
 bin/interproscan-5.31-70.0/interproscan.sh \
---input output/asw_timecourse/interproscan/unchar_hypo_annot_degs.fasta \
+--input output/asw_timecourse/interproscan/interproscan_degs.fasta \
 --seqtype n \
 --output-dir output/asw_timecourse/interproscan \
 --cpu 50 \
