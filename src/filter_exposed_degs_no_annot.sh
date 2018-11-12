a20834c8fa6f096616bae4e8874f0564f36c55b8
Sarah Inwood <sninwood@gmail.com>
2018-11-12 16:10:05 +1300

    editing filtering scripts

--- src/filter_exposed_degs_no_annot.sh
+++ src/filter_exposed_degs_no_annot.sh
@@ -3,7 +3,7 @@
 set -eu
 
 bin/bbmap/filterbyname.sh \
-in=data/asw_Trinity.fasta \
+in=data/Trinity.fasta \
 include=t \
 names=output/exposed/deseq2/degs_with_no_annot.txt \
 out=output/exposed/no_annot/degs_no_annot.fasta
