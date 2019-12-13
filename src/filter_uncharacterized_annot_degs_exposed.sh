306a1e750ffb01ad7feb46fa6421bfcf8c814552
Sarah Inwood <sninwood@gmail.com>
2019-12-13 14:41:53 +1300

    organising scripts into folders

--- src/filter_uncharacterized_annot_degs_exposed.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-bin/bbmap/filterbyname.sh \
-in=data/asw_transcriptome/Trinity.fasta \
-include=t \
-names=output/exposed/interproscan/interproscan_ids.txt \
-out=output/exposed/interproscan/interproscan_degs.fasta
--- src/filter_uncharacterized_annot_degs_timecourse.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-bin/bbmap/filterbyname.sh \
-in=data/asw_transcriptome/Trinity.fasta \
-include=t \
-names=output/asw_timecourse/interproscan/interproscan_ids.txt \
-out=output/asw_timecourse/interproscan/interproscan_degs.fasta
